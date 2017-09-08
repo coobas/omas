03af648f5ae44fd031af5fa7e2f86834bf3802b7
Orso Meneghini <meneghini@fusion.gat.com>
2017-09-07 23:47:42 -0700

    Added IMAS array structure allocation functionality

--- omas_imas.py
+++ omas_imas.py
@@ -16,35 +16,49 @@ def imas_open(user,tokamak,version,shot,run,new=False):
         raise(Exception('Failed to establish connection to IMAS database (user:%s tokamak:%s version:%s shot:%s run:%s)'%(user,tokamak,version,shot,run)))
     return ids
 
-def imas_set(ids,path,value,skipMissingNodes=False):
+def imas_set(ids,path,value,skipMissingNodes=False,allocate=False):
     printd('setting: %s'%repr(path),topic='imas')
     ds=path[0]
     path=path[1:]
 
-    m=getattr(ids,ds)
-    m.setExpIdx(0)
-
-    out=m
-    for p in path[:-1]:
-        if isinstance(p,basestring):
-            out=getattr(out,p)
-        else:
-            try:
-                out=out[p]
-            except IndexError:
-                printd('resizing: %d'%(p+1),topic='imas')
-                out.resize(2) #issue:resizing must be done once before data write
-                out=out[p]
-    if hasattr(out,path[-1]):
-        setattr(out,path[-1],value)
-        m.put(0)
-        return [ds]+path
+    if hasattr(ids,ds):
+        m=getattr(ids,ds)
     elif skipMissingNodes is not False:
         if skipMissingNodes is None:
             printe('WARNING: %s is not part of IMAS structure'%human_redeable_path([ds]+path))
         return None
     else:
         raise(AttributeError('%s is not part of IMAS structure'%human_redeable_path([ds]+path)))
+    m.setExpIdx(0)
+
+    out=m
+    for kp,p in enumerate(path):
+        if isinstance(p,basestring):
+            if hasattr(out,p):
+                if kp<(len(path)-1):
+                    out=getattr(out,p)
+            elif skipMissingNodes is not False:
+                if skipMissingNodes is None:
+                    printe('WARNING: %s is not part of IMAS structure'%human_redeable_path([ds]+path))
+                return None
+            else:
+                raise(AttributeError('%s is not part of IMAS structure'%human_redeable_path([ds]+path)))
+        else:
+            try:
+                out=out[p]
+            except IndexError:
+                if not allocate:
+                    raise(IndexError('%s structure array exceed allocation'%human_redeable_path([ds]+path)))
+                printd('resizing: %d'%(p+1),topic='imas')
+                out.resize(p+1)
+                out=out[p]
+
+    if allocate:
+        return [ds]+path
+
+    setattr(out,path[-1],value)
+    m.put(0)
+    return [ds]+path
 
 def imas_get(ids,path,skipMissingNodes=False):
     printd('fetching: %s'%repr(path),topic='imas')
@@ -63,7 +77,7 @@ def imas_get(ids,path,skipMissingNodes=False):
     m.get()
 
     out=m
-    for p in path:
+    for kp,p in enumerate(path):
         if isinstance(p,basestring):
             if hasattr(out,p):
                 out=getattr(out,p)
@@ -89,18 +103,23 @@ def save_omas_imas(ods, user, tokamak, version, shot, run, new=False):
     paths=htraverse(hierarchy)[0]
 
     ids=imas_open(user, tokamak, version, shot, run, new)
+
     set_paths=[]
     for path in paths:
+        set_paths.append( hmas_set(ids,path,hierarchy,None,allocate=True) )
+    set_paths=filter(None,set_paths)
+
+    for path in set_paths:
         if 'time' in path[:1] or path[-1]!='time':
             continue
         printd('writing %s'%human_redeable_path(path))
-        set_paths.append( hmas_set(ids,path,hierarchy,True) )
-    for path in paths:
+        hmas_set(ids,path,hierarchy,True)
+    for path in set_paths:
         if 'time' in path[:1] or path[-1]=='time':
             continue
         printd('writing %s'%human_redeable_path(path))
-        set_paths.append( hmas_set(ids,path,hierarchy,None) )
-    return filter(None,set_paths)
+        hmas_set(ids,path,hierarchy,True)
+    return set_paths
 
 #------------------------------
 if __name__ == '__main__':
